53abff16a5bea08605d496a51e0a7a288cb309b1
Andrew14 <zet0173@mail.ru>
2013-10-01 15:39:42 +0600

    Initial commit

---
+++ WindowsApplication1/WindowsApplication1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y);
+        }
+    }
+}
