2d3857e1b290f2d5fd78585ceb5888c28c82a53e
Andrew14 <zet0173@mail.ru>
2013-10-08 15:47:09 +0600

    Store and draw crosses

--- WindowsApplication1/WindowsApplication1/Form1.cs
+++ WindowsApplication1/WindowsApplication1/Form1.cs
@@ -11,6 +11,8 @@ namespace WindowsApplication1
 {
     public partial class Form1 : Form
     {
+        List<Cross> Points = new List<Cross>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,16 @@ namespace WindowsApplication1
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
             this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y);
+            Points.Add(new Cross(e.X,e.Y));
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Cross count in this.Points)
+            {
+                count.ReDraw(e.Graphics);
+            }
         }
     }
 
@@ -26,11 +38,18 @@ namespace WindowsApplication1
     {
         int x, y;
 
+        Pen p = new Pen(Color.Black);
+
         public Cross(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
-    }
 
+        public void ReDraw(Graphics a)
+        {
+            a.DrawLine(p, this.x - 2, this.y - 2, this.x + 2, this.y + 2);
+            a.DrawLine(p, this.x - 2, this.y + 2, this.x + 2, this.y - 2);
+        }
+    }
 }
